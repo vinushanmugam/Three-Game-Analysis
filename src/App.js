d6089dd7390a18764779024f46bfcece9144d4dd
Vinu Shanmugam <vinu.sampathk@gmail.com>
2026-03-06 10:10:34 -0800

    Updates for Fix ESLint unused vars and prepare for clean build

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { useState } from "react";
-
+/* eslint-disable no-unused-vars */ 
 // ─── RAW GAME DATA ────────────────────────────────────────────────────────────
 const GAMES = {
   detVsNyk: {
